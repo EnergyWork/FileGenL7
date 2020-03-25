c384c3dd40a935866fda7f2b77b064841ff5f696
Artyom <45438330+EnergyWork@users.noreply.github.com>
2020-03-25 23:25:09 +0300

    upd

--- FileGenL7/Main.cpp
+++ FileGenL7/Main.cpp
@@ -1,8 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <fstream>
 using namespace std;
 
 int main() {
-
+	setlocale(LC_NUMERIC, "ru_RU.utf8");
+	FILE* ptrFile;
+	ptrFile = fopen("fileL7.txt", "w");
+	for (size_t i = 0; i < 10000; i++) {
+		double x = double(rand() % 15) / 3.;
+		double y = double(rand() % 15) / 3.;
+		int b = (x >= 1 && x <= 4 && y >= 0 && y <= 5) && !(x > 2 && x < 3 && y > 1 && y < 4);
+		fprintf(ptrFile, "%f %f %d\n", x, y, b);
+	}
+	fclose(ptrFile);
 	return 0;
 }
